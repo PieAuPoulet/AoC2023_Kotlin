dd3fcd834fb381efbf8f116c29fd15e62a6113fd
Antoine <antoine.niot@gmail.com>
2023-12-15 10:00:21 +0000

    Day 1
    Taking it from the official video

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
+import com.sun.jdi.IntegerType
+
 fun main() {
+
+    val input = readInput("Day01")
+    val day01 = Day01()
+    day01.part1(input).println()
+    day01.part2(input).println()
+}
+
+class Day01
+{
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+                line : String -> line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line : String -> getNumber(line, StartFrom.BEGIN) * 10 + getNumber(line, StartFrom.END)};
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    enum class StartFrom {BEGIN, END}
+    val words = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    fun getNumber(line: String, startFrom: StartFrom): Int
+    {
+        val indices = when (startFrom)
+        {
+            StartFrom.BEGIN -> line.indices
+            StartFrom.END -> line.lastIndex downTo 0
+        }
+        for (index in indices)
+        {
+            line[index].digitToIntOrNull()?.let { return it }
+            for((wordIndex, word) in words.withIndex())
+            {
+                if(line.substring(index).startsWith(word))
+                    return wordIndex + 1
+            }
+        }
+        return 0
+    }
 }
+
+
